930618dd447d9655da80369fa1d02df6e8f616f3
胡小根 <hxg@haomo-studio.com>
2020-02-04 20:35:10 +0800

    完成uniapp页面的代码生成

---
+++ code/index.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="box">
+    <div class="bd">
+      <img class="layer" :src="constants.image1" />
+      <div class="bgWrap">
+        <img class="bg" :src="constants.image2" />
+        <div class="group">
+          <div class="container">
+            <img class="icon" :src="constants.image3" />
+            <span class="sketch">Sketch</span>
+            <img class="icon_2" :src="constants.image4" />
+            <span class="text">9:41 AM</span>
+          </div>
+          <div class="block">
+            <span class="percent">100%</span>
+            <img class="icon_3" :src="constants.image5" />
+          </div>
+        </div>
+        <div class="block_2">
+          <img class="icon_4" :src="constants.image6" />
+          <span class="dashboard">Dashboard</span>
+          <div class="group_2">
+            <img class="icon_5" :src="constants.image7" />
+            <img class="dot" :src="constants.image8" />
+          </div>
+          <div class="container_2">
+            <img class="icon_6" :src="constants.image9" />
+            <img class="dot_2" :src="constants.image10" />
+          </div>
+        </div>
+        <div class="group_3">
+          <div class="block_3">
+            <div class="outer">
+              <div class="statusWrap"><span class="status">Status</span></div>
+              <span class="summary">Summary</span>
+            </div>
+            <div class="colorDiv" />
+            <span class="announcement">Announcement</span>
+          </div>
+        </div>
+        <div class="block_4">
+          <div class="group_4">
+            <img class="icon_7" :src="constants.image11" />
+          </div>
+          <span class="bellamsystraatStreet">Bellamsystraat Street, CT</span>
+        </div>
+        <div class="group_5">
+          <div class="container_3"><div class="color" /></div>
+        </div>
+        <div class="sendReportWrap">
+          <span class="sendReport">Send Report</span>
+        </div>
+      </div>
+    </div>
+    <div class="block_5">
+      <img class="img" :src="constants.image12" />
+      <img class="image" :src="constants.image13" />
+    </div>
+    <div class="main">
+      <div class="block_7">
+        <div class="group_6"><span class="count">1.2</span></div>
+        <div class="empty" />
+        <div class="block_6"><span class="num">4.1</span></div>
+        <div class="div" />
+        <div class="group_7"><span class="count_2">7.2</span></div>
+        <div class="empty_2" />
+      </div>
+      <div class="block_11">
+        <div class="actionBgWrap">
+          <img class="actionBg" :src="constants.image14" />
+          <div class="block_9">
+            <span class="wilhelminastraat">Wilhelminastraat</span>
+            <div class="block_8">
+              <img class="icon_8" :src="constants.image15" />
+              <img class="icon_9" :src="constants.image16" />
+            </div>
+          </div>
+          <span class="info">1.2 Km from your current location.</span>
+          <span class="info_2">20 mins without traffic</span>
+        </div>
+        <div class="group_8">
+          <span class="hudsontraat">Hudsontraat</span>
+          <span class="info_3">4.1 Km from your current location.</span>
+          <span class="info_4">53 mins without traffic</span>
+        </div>
+        <div class="block_10">
+          <span class="overtoom">Overtoom</span>
+          <span class="info_5">7.2 Km from your current location.</span>
+          <span class="info_6">1 hour 15 mins without traffic</span>
+        </div>
+      </div>
+      <div class="block_12"><div class="color_2" /></div>
+    </div>
+    <div class="ft">
+      <div class="block_13">
+        <div class="assignmentWrap">
+          <span class="assignment">Assignment</span>
+        </div>
+        <span class="jobQueue">Job Queue</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      constants: {
+        image1:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/0c60fd5046d611ea857481b7fe145c48.png',
+        image2: 'https://via.placeholder.com/375x278text=375x278',
+        image3:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/6b93b79046d611eaa7cc5f786a9c0dd4.png',
+        image4:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/6bbebf3046d611eaa00ce1ab66a1de75.png',
+        image5:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/6bf81eb046d611eab758f39ace46119b.png',
+        image6:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/6a66a94046d611eaa7ebf7bd350c0c46.png',
+        image7:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/6b16e67046d611ea8b386b72f515a33a.png',
+        image8:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/6ab31b9046d611ea82a00d7d610ea15e.png',
+        image9:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/6b3fcb3046d611eaadb621dac880eba4.png',
+        image10:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/6b68aff046d611ea9f2387760fbedd51.png',
+        image11: 'https://via.placeholder.com/12x14text=12x14',
+        image12: 'https://via.placeholder.com/8x5text=8x5',
+        image13: 'https://via.placeholder.com/8x5text=8x5',
+        image14: 'https://via.placeholder.com/285x85text=285x85',
+        image15: 'https://via.placeholder.com/25x25text=25x25',
+        image16:
+          'https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/0856719046d611ea8cc4897af9967148.png'
+      }
+    };
+  },
+  methods: {}
+};
+</script>
+<style>
+@import './index.response.css';
+</style>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,63 @@
 module.exports = function(schema, option) {
-  const {prettier} = option;
+  const {_, prettier} = option;
+
+  // template
+  const template = [];
 
   // imports
   const imports = [];
 
-  // inline style
-  const style = {};
-
   // Global Public Functions
   const utils = [];
 
-  // Classes 
-  const classes = [];
+  // data
+  const datas = [];
 
-  // 1vw = width / 100
-  const _w = option.responsive.width / 100;
+  const constants = {};
+
+  // methods
+  const methods = [];
+
+  const expressionName = [];
+
+  // lifeCycles
+  const lifeCycles = [];
+
+  // styles
+  const styles = [];
+
+  const styles4vw = [];
+
+  // box relative style
+  const boxStyleList = ['fontSize', 'marginTop', 'marginBottom', 'paddingTop', 'paddingBottom', 'height', 'top', 'bottom', 'width', 'maxWidth', 'left', 'right', 'paddingRight', 'paddingLeft', 'marginLeft', 'marginRight', 'lineHeight', 'borderBottomRightRadius', 'borderBottomLeftRadius', 'borderTopRightRadius', 'borderTopLeftRadius', 'borderRadius'];
+
+  // no unit style
+  const noUnitStyles = ['opacity', 'fontWeight'];
+
+  const lifeCycleMap = {
+    '_constructor': 'onLoad',
+    'getDerivedStateFromProps': '',
+    'render': '',
+    'componentDidMount': 'onReady',
+    'componentDidUpdate': 'onShow',
+    'componentWillUnmount': 'onUnload'
+  }
+
+  const width = option.responsive.width || 750;
+  const viewportWidth = option.responsive.viewportWidth || 375;
+
+  // 1rpx = 750 / width
+  const _w = ( 750 / width);
+  console.log(`_w: ${_w}`);
+  const _ratio = width / viewportWidth;
+  console.log(`_ratio: ${_ratio}`);
 
   const isExpression = (value) => {
     return /^\{\{.*\}\}$/.test(value);
+  }
+
+  const transformEventName = (name) => {
+    return name.replace('on', '').toLowerCase();
   }
 
   const toString = (value) => {
@@ -41,56 +81,47 @@ module.exports = function(schema, option) {
   };
 
   // convert to responsive unit, such as vw
-  const parseStyle = (style) => {
+  const parseStyle = (style, toVW) => {
+    const styleData = [];
     for (let key in style) {
-      switch (key) {
-        case 'fontSize':
-        case 'marginTop':
-        case 'marginBottom':
-        case 'paddingTop':
-        case 'paddingBottom':
-        case 'height':
-        case 'top':
-        case 'bottom':
-        case 'width':
-        case 'maxWidth':
-        case 'left':
-        case 'right':
-        case 'paddingRight':
-        case 'paddingLeft':
-        case 'marginLeft':
-        case 'marginRight':
-        case 'lineHeight':
-        case 'borderBottomRightRadius':
-        case 'borderBottomLeftRadius':
-        case 'borderTopRightRadius':
-        case 'borderTopLeftRadius':
-        case 'borderRadius':
-          style[key] = (parseInt(style[key]) / _w).toFixed(2) + 'vw';
-          break;
+      let value = style[key];
+      if (boxStyleList.indexOf(key) != -1) {
+        if (toVW) {
+          value = (parseInt(value) / _w).toFixed(2);
+          value = value == 0 ? value : value + 'rpx';
+        } else {
+          value = (parseInt(value)).toFixed(2);
+          value = value == 0 ? value : value + 'px';
+        }
+        styleData.push(`${_.kebabCase(key)}: ${value}`);
+      } else if (noUnitStyles.indexOf(key) != -1) {
+        styleData.push(`${_.kebabCase(key)}: ${parseFloat(value)}`);
+      } else {
+        styleData.push(`${_.kebabCase(key)}: ${value}`);
       }
     }
-
-    return style;
+    return styleData.join(';');
   }
 
   // parse function, return params and content
   const parseFunction = (func) => {
     const funcString = func.toString();
+    const name = funcString.slice(funcString.indexOf('function'), funcString.indexOf('(')).replace('function ','');
     const params = funcString.match(/\([^\(\)]*\)/)[0].slice(1, -1);
     const content = funcString.slice(funcString.indexOf('{') + 1, funcString.lastIndexOf('}'));
     return {
       params,
-      content
+      content,
+      name
     };
   }
 
   // parse layer props(static values or expression)
-  const parseProps = (value, isReactNode) => {
+  const parseProps = (value, isReactNode, constantName) => {
     if (typeof value === 'string') {
       if (isExpression(value)) {
         if (isReactNode) {
-          return value.slice(1, -1);
+          return `{{${value.slice(7, -2)}}}`;
         } else {
           return value.slice(2, -2);
         }
@@ -98,12 +129,27 @@ module.exports = function(schema, option) {
 
       if (isReactNode) {
         return value;
+      } else if (constantName) { // save to constant
+        expressionName[constantName] = expressionName[constantName] ? expressionName[constantName] + 1 : 1;
+        const name = `${constantName}${expressionName[constantName]}`;
+        constants[name] = value;
+        return `"constants.${name}"`;
       } else {
-        return `'${value}'`;
+        return `"${value}"`;
       }
     } else if (typeof value === 'function') {
-      const {params, content} = parseFunction(value);
-      return `(${params}) => {${content}}`;
+      const {params, content, name} = parseFunction(value);
+      expressionName[name] = expressionName[name] ? expressionName[name] + 1 : 1;
+      methods.push(`${name}_${expressionName[name]}(${params}) {${content}}`);
+      return `${name}_${expressionName[name]}`;
+    }
+  }
+
+  const parsePropsKey = (key, value) => {
+    if (typeof value === 'function') {
+      return `@${transformEventName(key)}`;
+    } else {
+      return `:${key}`;
     }
   }
 
@@ -116,8 +162,8 @@ module.exports = function(schema, option) {
 
     switch (action) {
       case 'fetch':
-        if (imports.indexOf(`import {fetch} from whatwg-fetch`) === -1) {
-          imports.push(`import {fetch} from 'whatwg-fetch'`);
+        if (imports.indexOf(`import {request, fetch} from '@/common/request'`) === -1) {
+          imports.push(`import {request, fetch} from '@/common/request'`);
         }
         payload = {
           method: method
@@ -165,11 +211,15 @@ module.exports = function(schema, option) {
 
   // parse condition: whether render the layer
   const parseCondition = (condition, render) => {
-    if (typeof condition === 'boolean') {
-      return `${condition} && ${render}`
-    } else if (typeof condition === 'string') {
-      return `${condition.slice(2, -2)} && ${render}`
-    }
+    const tagEnd = render.indexOf('>');
+    let _condition = isExpression(condition) ? condition.slice(2, -2) : condition;
+    _condition = _condition.replace('this.', '');
+    render = `
+      ${render.slice(0, tagEnd)}
+      v-if="${_condition}"  
+      ${render.slice(tagEnd)}`;
+
+    return render;
   }
 
   // parse loop render
@@ -179,32 +229,44 @@ module.exports = function(schema, option) {
     let loopArgIndex = (loopArg && loopArg[1]) || 'index';
 
     if (Array.isArray(loop)) {
-      data = toString(loop);
+      data = 'loopData';
+      datas.push(`${data}: ${toString(loop)}`);
     } else if (isExpression(loop)) {
-      data = loop.slice(2, -2);
+      data = loop.slice(2, -2).replace('this.state.', '');
     }
-
     // add loop key
-    const tagEnd = render.match(/^<.+?\s/)[0].length;
-    render = `${render.slice(0, tagEnd)} key={${loopArgIndex}}${render.slice(tagEnd)}`;
+    const tagEnd = render.indexOf('>');
+    const keyProp = render.slice(0, tagEnd).indexOf('key=') == -1 ? `:key="${loopArgIndex}"` : '';
+    render = `
+      ${render.slice(0, tagEnd)}
+      v-for="(${loopArgItem}, ${loopArgIndex}) in ${data}"  
+      ${keyProp}
+      ${render.slice(tagEnd)}`;
 
     // remove `this` 
     const re = new RegExp(`this.${loopArgItem}`, 'g')
     render = render.replace(re, loopArgItem);
 
-    return `${data}.map((${loopArgItem}, ${loopArgIndex}) => {
-      return (${render});
-    })`;
+    return render;
   }
 
   // generate render xml
   const generateRender = (schema) => {
     const type = schema.componentName.toLowerCase();
     const className = schema.props && schema.props.className;
-    const classString = className ? ` style={styles.${className}}` : '';
+    const classString = className ? ` class="${className}"` : '';
 
     if (className) {
-      style[className] = parseStyle(schema.props.style);
+      styles.push(`
+        .${className} {
+          ${parseStyle(schema.props.style)}
+        }
+      `);
+      styles4vw.push(`
+        .${className} {
+          ${parseStyle(schema.props.style, true)}
+        }
+      `);
     }
 
     let xml;
@@ -212,22 +274,23 @@ module.exports = function(schema, option) {
 
     Object.keys(schema.props).forEach((key) => {
       if (['className', 'style', 'text', 'src'].indexOf(key) === -1) {
-        props += ` ${key}={${parseProps(schema.props[key])}}`;
+        props += ` ${parsePropsKey(key, schema.props[key])}="${parseProps(schema.props[key])}"`;
       }
     })
 
     switch(type) {
       case 'text':
         const innerText = parseProps(schema.props.text, true);
-        xml = `<span${classString}${props}>${innerText}</span>`;
+        xml = `<span${classString}${props}>${innerText}</span> `;
         break;
       case 'image':
-        const source = parseProps(schema.props.src);
-        xml = `<img${classString}${props} src={${source}} />`;
+        const source = parseProps(schema.props.src, false, 'image');
+        xml = `<img${classString}${props} :src=${source} /> `;
         break;
       case 'div':
       case 'page':
       case 'block':
+      case 'component':
         if (schema.children && schema.children.length) {
           xml = `<div${classString}${props}>${transform(schema.children)}</div>`;
         } else {
@@ -241,9 +304,6 @@ module.exports = function(schema, option) {
     }
     if (schema.condition) {
       xml = parseCondition(schema.condition, xml);
-    }
-    if (schema.loop || schema.condition) {
-      xml = `{${xml}}`;
     }
 
     return xml;
@@ -260,17 +320,12 @@ module.exports = function(schema, option) {
     } else {
       const type = schema.componentName.toLowerCase();
 
-      if (['page', 'block'].indexOf(type) !== -1) {
+      if (['page', 'block', 'component'].indexOf(type) !== -1) {
         // 容器组件处理: state/method/dataSource/lifeCycle/render
-        const states = [];
-        const lifeCycles = [];
-        const methods = [];
         const init = [];
-        const render = [`render(){ return (`];
-        let classData = [`class ${schema.componentName}_${classes.length} extends Component {`];
 
         if (schema.state) {
-          states.push(`state = ${toString(schema.state)}`);
+          datas.push(`${toString(schema.state).slice(1, -1)}`);
         }
 
         if (schema.methods) {
@@ -299,27 +354,23 @@ module.exports = function(schema, option) {
 
         if (schema.lifeCycles) {
           if (!schema.lifeCycles['_constructor']) {
-            lifeCycles.push(`constructor(props, context) { super(); ${init.join('\n')}}`);
+            lifeCycles.push(`${lifeCycleMap['_constructor']}() { ${init.join('\n')}}`);
           }
 
           Object.keys(schema.lifeCycles).forEach((name) => {
+            const vueLifeCircleName = lifeCycleMap[name] || name;
             const { params, content } = parseFunction(schema.lifeCycles[name]);
 
             if (name === '_constructor') {
-              lifeCycles.push(`constructor(${params}) { super(); ${content} ${init.join('\n')}}`);
+              lifeCycles.push(`${vueLifeCircleName}() {${content} ${init.join('\n')}}`);
             } else {
-              lifeCycles.push(`${name}(${params}) {${content}}`);
+              lifeCycles.push(`${vueLifeCircleName}() {${content}}`);
             }
           });
         }
 
-        render.push(generateRender(schema))
-        render.push(`);}`);
+        template.push(generateRender(schema));
 
-        classData = classData.concat(states).concat(lifeCycles).concat(methods).concat(render);
-        classData.push('}');
-
-        classes.push(classData.join('\n'));
       } else {
         result += generateRender(schema);
       }
@@ -336,35 +387,62 @@ module.exports = function(schema, option) {
 
   // start parse schema
   transform(schema);
+  datas.push(`constants: ${toString(constants)}`);
 
   const prettierOpt = {
-    parser: 'babel',
-    printWidth: 120,
+    parser: 'vue',
+    printWidth: 80,
     singleQuote: true
   };
 
   return {
     panelDisplay: [
       {
-        panelName: `index.jsx`,
+        panelName: `index.vue`,
         panelValue: prettier.format(`
-          'use strict';
-
-          import React, { Component } from 'react';
-          ${imports.join('\n')}
-          import styles from './style.js';
-          ${utils.join('\n')}
-          ${classes.join('\n')}
-          export default ${schema.componentName}_0;
+          <template>
+              ${template}
+          </template>
+          <script>
+            ${imports.join('\n')}
+            export default {
+              data() {
+                return {
+                  ${datas.join(',\n')}
+                } 
+              },
+              methods: {
+                ${methods.join(',\n')}
+              },
+              ${lifeCycles.join(',\n')}
+            }
+          </script>
+          <style>
+          @import "./index.response.css";
+          </style>
         `, prettierOpt),
-        panelType: 'js',
+        panelType: 'vue',
       },
       {
-        panelName: `style.js`,
-        panelValue: prettier.format(`export default ${toString(style)}`, prettierOpt),
-        panelType: 'js'
+        panelName: 'index.css',
+        panelValue: prettier.format(`${styles.join('\n')}`, {parser: 'css'}),
+        panelType: 'css'
+      },
+      {
+        panelName: 'index.response.css',
+        panelValue: prettier.format(styles4vw.join('\n'), {parser: 'css'}),
+        panelType: 'css'
       }
     ],
+    renderData: {
+      template: template,
+      imports: imports,
+      datas: datas,
+      methods: methods,
+      lifeCycles: lifeCycles,
+      styles: styles
+
+    },
     noTemplate: true
   };
 }
